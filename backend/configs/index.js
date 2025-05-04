55ad70ae98d430b9e3c69dcd58de4622cb4dcad6
ShahMir99 <chand14ok@gmail.com>
2025-05-04 17:02:15 +0500

    change .env location

--- backend/configs/index.js
+++ backend/configs/index.js
@@ -1,13 +1,11 @@
 import * as dotenv from "dotenv";
-import path from 'path';
-
-dotenv.config({ path: path.resolve('./backend/.env') });
+dotenv.config();
 
 export const Config = {
   jwtSecret : process.env.JWT_SECRET,
   databaseUrl : process.env.DATABASE_URI,
   databaseName : process.env.DATABASE_NAME,
-  appPort : process.env.DATABASE_PORT || 5000,
+  appPort : process.env.PORT || 5000,
 
   clientUrl : process.env.CLIENT_URL,
 
